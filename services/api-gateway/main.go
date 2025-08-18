b4a6b5f0edd54041095e478b78758ea1f4faa1ba
Mohamed Samir <gm.mohamedsamir@gmail.com>
2025-08-18 20:19:38 +0300

    add gracefull shutdown

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
 	"ride-sharing/shared/env"
+	"syscall"
+	"time"
 )
 
 var (
@@ -11,8 +16,6 @@ var (
 )
 
 func main() {
-	slog.Info("Starting API Gateway")
-
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /trip/preview", handleTripPreview)
@@ -22,7 +25,28 @@ func main() {
 		Handler: mux,
 	}
 
-	if err := srv.ListenAndServe(); err != nil {
-		slog.Error("Failed to start API Gateway", "error", err)
+	errCh := make(chan error, 1)
+	go func() {
+		slog.Info("starting API Gateway server", "port", httpAddr)
+		errCh <- srv.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-errCh:
+		slog.Error("failed to start the server", "error", err)
+
+	case sig := <-shutdown:
+		slog.Warn("server is shutting down", "signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			slog.Error("couldn't stop the server gracefully", "error", err)
+			srv.Close()
+		}
 	}
 }
--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
 	handlers "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
 	"ride-sharing/shared/env"
+	"syscall"
+	"time"
 )
 
 var (
@@ -27,8 +32,27 @@ func main() {
 		Handler: mux,
 	}
 
-	if err := srv.ListenAndServe(); err != nil {
-		slog.Error("Failed to start server", "error", err)
-		return
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- srv.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-errCh:
+		slog.Error("Failed to start the server", "error", err)
+
+	case sig := <-shutdown:
+		slog.Warn("server is shutting down", "signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			slog.Error("failed to shutdown the server gracefully", "error", err)
+			srv.Close()
+		}
 	}
 }
